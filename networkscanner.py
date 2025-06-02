a84c563fea149d62f0ce05e20d57445cd35db98e
VISHAAL-KUMAR-A <107105545+VISHAAL-KUMAR-A@users.noreply.github.com>
2025-06-03 01:26:39 +0530

    Update networkscanner.py

--- networkscanner.py
+++ networkscanner.py
@@ -1,25 +1,21 @@
-import scapy
+import scapy.all as scapy
 import optparse
-
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.ETHER(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast,
-                              timeout=5, verbose=False)[0]
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=5, verbose=False)[0]
     print("IP address\t\tMAC address\n-------------------------------------------------------")
     for element in answered_list:
         print(f"{element[1].psrc}\t\t{element[1].hwsrc}")
 
-def get_arguements():
+def get_arguments():
     parser = optparse.OptionParser()
-    parser.add_option("-i", "ip", dest="ip", help="Target IP/IP range")
-    (options, arguements) = parser.parse_args()
+    parser.add_option("-i", "--ip", dest="ip", help="Target IP/IP range")
+    (options, arguments) = parser.parse_args()
     if not options.ip:
-        parser.error(
-            "[-] Please specify an IP address,use --help for more info")
+        parser.error("[-] Please specify an IP address, use --help for more info")
     return options
 
-
-scan(get_arguements().ip)
+scan(get_arguments().ip)
